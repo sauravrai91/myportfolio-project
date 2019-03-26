7eb45a1597a3b7992f147e4c4be72decd06839a3
Saurav Rai <saurav.rai@ad.infosys.com>
2019-03-26 16:56:53 +0530

    Website up and running

--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,15 @@
 from django.db import models
 
+
 # Create your models here.
 class Blog(models.Model):
-    title = models.CharField(max_length = 100)
+    title = models.CharField(max_length=100)
     pub_date = models.DateField()
-    body = models.TextField(max_length = 1200)
+    body = models.TextField(max_length=1200)
     image = models.ImageField(upload_to='images/')
+
+    def __str__(self):
+        return self.title
+    
+    def summary(self):
+        return self.body[:120]+"..."
